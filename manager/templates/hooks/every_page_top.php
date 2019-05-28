CharlieandDavid/redcap-external-modules

commit 3f85d35daa30c6059b556b8c8f83cd08348a174b

Fixed broken project link icons in 9.0.1

manager/templates/hooks/every_page_top.php
@@ -30,10 +30,17 @@ $menu_id = 'projMenuExternalModules';
 
 			function getLink(icon, name,url, target){
 				newLink = exampleLink.clone()
-				newLink.find('img').attr('src', icon)
 				newLink.find('a').attr('href', url+'&pid=<?= $project_id ?>')
 				newLink.find('a').attr('target', target)
 				newLink.find('a').html(name);
+
+				var img = $('<img />')
+				img.css('vertical-align', '-3px')
+				img.css('height', '14px')
+				img.css('width', '14px')
+				img.attr('src', icon)
+				newLink.find('i').replaceWith(img)
+
 				return(newLink);
 			}
 

commit 799b62522bd6bd171ea26501e2033765c55d32d3

Fixed icons broken by 3f85d35 in LTS releases

manager/templates/hooks/every_page_top.php
@@ -34,12 +34,20 @@ $menu_id = 'projMenuExternalModules';
 				newLink.find('a').attr('target', target)
 				newLink.find('a').html(name);
 
-				var img = $('<img />')
-				img.css('vertical-align', '-3px')
-				img.css('height', '14px')
-				img.css('width', '14px')
-				img.attr('src', icon)
-				newLink.find('i').replaceWith(img)
+				var fontAwesomeIcon = newLink.find('i')
+				if(fontAwesomeIcon.length === 0){
+					// We're in 9.0.1+
+					newLink.find('img').attr('src', icon)
+				}
+				else{
+					// We're in an LTS release prior to 9.0.1
+					var img = $('<img />')
+					img.css('vertical-align', '-3px')
+					img.css('height', '14px')
+					img.css('width', '14px')
+					img.attr('src', icon)
+					newLink.find('i').replaceWith(img)
+				}
 
 				return(newLink);
 			}