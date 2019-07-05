434347df32a26cf4b7353ba4babc7eb4036cc376
Scott J. Pearson <scott.j.pearson@vumc.org>
2019-07-05 11:25:10 -0500

    Bug fix for test routine

--- tests/ExternalModulesTest.php
+++ tests/ExternalModulesTest.php
@@ -100,7 +100,6 @@ class ExternalModulesTest extends BaseTest
                 			'cron_hour' => date("G", $time),
                 			'cron_minute' => date("i", $time),
 					);
-			echo "($offset, $validationMethod) ";
 			$this->$validationMethod(self::callPrivateMethod($method, array_merge($defaultCron, $cron)));
 		}
 
@@ -110,6 +109,7 @@ class ExternalModulesTest extends BaseTest
 				'cron_minute' => date("i", $time2),
 				'cron_weekday' => date("w", $time2),
 				);
+		echo json_encode($cron2)."\n";
 		$this->assertFalse(self::callPrivateMethod($method, array_merge($defaultCron, $cron2)));
 
 		$time3 = time() + 7 * 24 * 3600;
@@ -118,6 +118,7 @@ class ExternalModulesTest extends BaseTest
 				'cron_minute' => date("i", $time3),
 				'cron_weekday' => date("w", $time3),
 				);
+		echo json_encode($cron3)."\n";
 		$this->assertTrue(self::callPrivateMethod($method, array_merge($defaultCron, $cron3)));
 
 		$cron3_2 = array(
@@ -125,6 +126,7 @@ class ExternalModulesTest extends BaseTest
 				'cron_minute' => date("i", $time3),
 				'cron_monthday' => date("j", $time3),
 				);
+		echo json_encode($cron3_2)."\n";
 		$this->assertFalse(self::callPrivateMethod($method, array_merge($defaultCron, $cron3_2)));
 	}
 
