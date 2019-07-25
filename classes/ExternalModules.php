5b6bf072e1c8fa4d8f28ebb4fde707d783f858ce
moorejr5 <james.r.moore@vanderbilt.edu>
2019-07-25 11:32:45 -0500

    Fix to stop system from creating an "ERROR_tmp" folder over and over again when a REDCap installation has a module that has had its approval_status set to 'No' in our Repo project.

--- classes/ExternalModules.php
+++ classes/ExternalModules.php
@@ -2776,7 +2776,7 @@ class ExternalModules
 		// Also obtain the folder name of the module
 		$moduleFolderName = http_get(APP_URL_EXTMOD_LIB . "download.php?module_id=$module_id&name=1");
 
-		if(empty($moduleFolderName)){
+		if(empty($moduleFolderName) || $moduleFolderName == "ERROR"){
 			throw new Exception("The request to retrieve the name for module $module_id from the repo failed.");
 		}
 
