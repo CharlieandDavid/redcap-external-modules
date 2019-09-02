95b6814d34e2e734a9319a7af25440f0cee53efc
Mark McEver <mark@fastmail.net>
2019-09-02 15:27:39 -0500

    Removed three old method stubs that (if implemented) should likely happen in the framework class now

--- classes/AbstractExternalModule.php
+++ classes/AbstractExternalModule.php
@@ -94,27 +94,6 @@ class AbstractExternalModule
 		return !preg_match("/[^a-z0-9-_]/", $key);
 	}
 
-	function selectData($some, $params=array())
-	{
-		self::checkPermissions(__FUNCTION__);
-
-		return 'this could be some data from the database';
-	}
-
-	function updateData($some, $params=array())
-	{
-		self::checkPermissions(__FUNCTION__);
-
-		throw new Exception('Not yet implemented!');
-	}
-
-	function deleteData($some, $params=array())
-	{
-		self::checkPermissions(__FUNCTION__);
-
-		throw new Exception('Not yet implemented!');
-	}
-
 	function updateUserPermissions($some, $params=array())
 	{
 		self::checkPermissions(__FUNCTION__);
