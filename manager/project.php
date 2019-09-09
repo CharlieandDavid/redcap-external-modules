9a39bacb1609b9430b919dc683d9cb449dc2f3c1
Mark McEver <mark@fastmail.net>
2019-09-09 12:09:45 -0500

    Added the current project to the module configuration dialog per community question 69945

--- manager/project.php
+++ manager/project.php
@@ -1,5 +1,8 @@
 <?php
 namespace ExternalModules;
+
+use REDCap;
+
 require_once __DIR__ . '/../classes/ExternalModules.php';
 require_once ExternalModules::getProjectHeaderPath();
 
@@ -34,11 +37,14 @@ ExternalModules::safeRequireOnce('manager/templates/enabled-modules.php');
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
 			</div>
 			<div class="modal-body">
+				<div style='font-size: 14px; color: #212529; margin-left: 12px;'>
+					<b>Project:</b> <?=REDCap::getProjectTitle()?>
+				</div>
 				<table class="table table-no-top-row-border">
 					<thead>
 						<tr>
-							<th>Project Settings</th>
-							<th style='text-align: center;'>Value</th>
+							<th>Settings</th>
+							<th style='text-align: center;'>Values</th>
 							<th style='min-width: 75px; text-align: center;'></th>
 						</tr>
 					</thead>
