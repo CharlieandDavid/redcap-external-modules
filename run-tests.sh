cd2b55e82de8f111f4c320d40c8952ee327cf0ac
Mark McEver <mark@fastmail.net>
2019-09-10 12:12:33 -0500

    Make the test script run even if composer wasn't in the path (like in cron jobs)

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-composer install
+if hash composer; then
+    composer install
+fi
 
+echo Running tests...
 vendor/bin/phpunit
+
+echo Ensuring PHP version compatibility...
 vendor/bin/phpcs -p --runtime-set testVersion 5.5- --standard=vendor/phpcompatibility/php-compatibility/PHPCompatibility --extensions=php --ignore=/vendor .
