063a97a02a7c84ed296fc96cd4aeccc95cdd2a34
Mark McEver <mark@fastmail.net>
2019-12-23 10:31:54 -0600

    Changed a query to use parameters

--- manager/templates/hooks/save_record.php
+++ manager/templates/hooks/save_record.php
@@ -6,6 +6,6 @@ $recordId = db_escape($arguments[1]);
 
 $temporaryRecordId = db_escape(@$_POST[ExternalModules::EXTERNAL_MODULES_TEMPORARY_RECORD_ID]);
 if(ExternalModules::isTemporaryRecordId($temporaryRecordId)){
-	$sql = "update redcap_external_modules_log set record = '$recordId' where record = '$temporaryRecordId'";
-	ExternalModules::query($sql);
+	$sql = "update redcap_external_modules_log set record = ? where record = ?";
+	ExternalModules::query($sql, [$recordId, $temporaryRecordId]);
 }
