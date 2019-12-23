904f3303f6d3fcde4657dddc0541ea64c62b6232
Mark McEver <mark@fastmail.net>
2019-12-23 13:40:57 -0600

    Moved the test module class to it's own file

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -8,7 +8,8 @@ if(!defined('PAGE')){
 	define('PAGE', 'unit testing');
 }
 
-require_once dirname(__FILE__) . '/../classes/ExternalModules.php';
+require_once __DIR__ . '/../classes/ExternalModules.php';
+require_once __DIR__ . '/BaseTestExternalModule.php';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -285,76 +286,5 @@ abstract class BaseTest extends TestCase
 
 	function __call($methodName, $args){
 		return call_user_func_array(array($this->getReflectionClass(), $methodName), $args);
-	}
-}
-
-class BaseTestExternalModule extends AbstractExternalModule {
-
-	public $testHookArguments;
-	private $settingKeyPrefix;
-
-	function __construct()
-	{
-		$this->PREFIX = TEST_MODULE_PREFIX;
-		$this->VERSION = TEST_MODULE_VERSION;
-
-		parent::__construct();
-	}
-
-	function getModulePath()
-	{
-		return __DIR__;
-	}
-
-	function redcap_test_delay($delayTestFunction)
-	{
-		// Although it perhaps shouldn't be, it is sometimes possible for getModuleInstance() to
-		// be called while inside a hook (it sometimes happens in the email alerts module).
-		// The getModuleInstance() function used to set the active module prefix to null on every call,
-		// which is problematic since the delayModuleExecution() method relies on the active prefix.
-		// This used to cause 'You must specify a prefix!' exceptions.
-		// We call getModuleInstance() inside this delay test hook to make sure this bug never reoccurs.
-		ExternalModules::getModuleInstance(TEST_MODULE_PREFIX);
-
-		$delayTestFunction($this->delayModuleExecution());
-	}
-
-	function redcap_test()
-	{
-		$this->testHookArguments = func_get_args();
-	}
-
-	function redcap_test_call_function($function = null){
-		// We must check if the arg is callable b/c it could be cron attributes for a cron job.
-		if(!is_callable($function)){
-			$function = $this->function;
-		}
-
-		$function();
-	}
-	
-	function redcap_every_page_test()
-	{
-		call_user_func_array([$this, 'redcap_test'], func_get_args());
-	}
-
-	function redcap_save_record()
-	{
-		$this->recordIdFromGetRecordId = $this->getRecordId();
-	}
-
-	protected function getSettingKeyPrefix()
-	{
-		if($this->settingKeyPrefix){
-			return $this->settingKeyPrefix;
-		}
-		else{
-			return parent::getSettingKeyPrefix();
-		}
-	}
-
-	function setSettingKeyPrefix($settingKeyPrefix)
-	{
-		$this->settingKeyPrefix = $settingKeyPrefix;
 	}
 }
