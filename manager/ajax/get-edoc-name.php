04214bba2cb649d0bf028049565dd94639645a97
Mark McEver <mark@fastmail.net>
2020-01-17 21:45:46 +0000

    Checked for design rights when retrieving edoc names

--- manager/ajax/get-edoc-name.php
+++ manager/ajax/get-edoc-name.php
@@ -1,13 +1,15 @@
-<?php
+<?php namespace ExternalModules;
 require_once dirname(dirname(dirname(__FILE__))) . '/classes/ExternalModules.php';
 require_once APP_PATH_DOCROOT.'Classes/Files.php';
+
+ExternalModules::requireDesignRights();
 
 $pid = @$_GET['pid'];
 $edoc = $_POST['edoc'];
 
 $doc_name = "";
 if (($edoc) && (is_numeric($edoc))) {
-	$ary = Files::getEdocContentsAttributes((integer) $edoc);
+	$ary = \Files::getEdocContentsAttributes((integer) $edoc);
 	$doc_name = $ary[1];
 }
 
