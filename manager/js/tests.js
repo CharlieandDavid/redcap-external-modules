f4abd0ca0e4b1e1fe73dbfbb8c0528f17a505c1a
Mark McEver <mark@fastmail.net>
2020-03-05 15:01:58 -0600

    Unit tested basic subsetting branching logic

--- manager/js/tests.js
+++ manager/js/tests.js
@@ -141,8 +141,17 @@ var ExternalModuleTests = {
     },
 
     assertDoBranching: function(expectedVisible, fieldValue, branchingLogic){
-        var fieldName1 = 'some_field_1'
+        var fieldName1 = 'some_field'
         var fieldName2 = 'some_other_field'
+        var fieldName3 = 'some_sub_field'
+        var subSettingsParentFieldName = 'sub_settings_parent'
+
+        var getInstanceInputName = function(field, instance){
+            return field + '____' + instance
+        }
+
+        var fieldName3instance1 = getInstanceInputName(fieldName3, 1)
+        var fieldName3instance2 = getInstanceInputName(fieldName3, 2)
 
         var conditions = branchingLogic.conditions
         if(conditions === undefined){
@@ -156,13 +165,23 @@ var ExternalModuleTests = {
         var settings = [
             {
                 key: fieldName1,
-                name: "Some Field",
-                value: fieldValue
+                name: "Some Field"
             },
             {
                 key: fieldName2,
                 name: "Some Other Field",
                 branchingLogic: branchingLogic
+            },
+            {
+                key: subSettingsParentFieldName,
+                type: 'sub_settings',
+                sub_settings: [
+                    {
+                        key: fieldName3,
+                        name: "Some Sub-Field",
+                        branchingLogic: branchingLogic
+                    }
+                ]
             }
         ]
 
@@ -170,19 +189,36 @@ var ExternalModuleTests = {
             // We're not defining ExternalModules.PID, so it's easier to test with system settings.
             'system-settings': settings
         }
-        
+
         var modal = $('#external-modules-configure-modal')
-        settings.forEach(function(setting){
-            modal.find('input[name=' + setting.key + ']').remove() // remove inputs from other assertions
-            modal.append('<input field="' + setting.key + '" name="' + setting.key + '" value="' + setting.value + '">')
-        })
+        var setupSetting = function(field, value, instance){
+            var name = field
+            if(instance !== undefined){
+                name = getInstanceInputName(field, instance)
+            }
+
+            modal.find('input[name=' + name + ']').remove() // remove inputs from other assertions
+            modal.append('<input field="' + field + '" name="' + name + '" value="' + value + '">\n')
+        }
+        
+        setupSetting(fieldName1, fieldValue)
+        setupSetting(fieldName2)
+        setupSetting(subSettingsParentFieldName)
+        setupSetting(fieldName3, null, 1)
+        setupSetting(fieldName3, null, 2)
         
         ExternalModules.Settings.prototype.doBranching()
 
-        var style = modal.find('[field='+fieldName2+']').attr('style')
-        var actuallyVisible = style !== 'display: none;'
+        var assert = function(fieldName){
+            var style = modal.find('[name='+fieldName+']').attr('style')
+            var actuallyVisible = style !== 'display: none;'
+    
+            ExternalModuleTests.assert(actuallyVisible === expectedVisible)
+        }
 
-        this.assert(actuallyVisible === expectedVisible)
+        assert(fieldName2)
+        assert(fieldName3instance1)
+        assert(fieldName3instance2)
 	},
 
 	assert: function(assertion){
