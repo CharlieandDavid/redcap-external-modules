36815908aaf6b14432d4f8dbe6cb257923289f0c
G. Rezniczek <g.rezniczek@gmail.com>
2020-03-05 19:10:50 +0100

    Fix get/setProjectSettings()

--- classes/ExternalModules.php
+++ classes/ExternalModules.php
@@ -315,19 +315,26 @@ class ExternalModules
 		return $settings;
 	}
 
+	// This is called from framework[v4]::setProjectSettings()
+	static function saveProjectSettings($moduleDirectoryPrefix, $pid, $settings)
+	{
+		self::setSettings($moduleDirectoryPrefix, $pid, $settings);
+	}
+
 	static function saveSettings($moduleDirectoryPrefix, $pid, $rawSettings)
 	{
 		$settings = self::formatRawSettings($moduleDirectoryPrefix, $pid, $rawSettings);
+		return self::setSettings($moduleDirectoryPrefix, $pid, $settings);
+	}
 
+	private static function setSettings($moduleDirectoryPrefix, $pid, $settings) {
 		$saveSqlByField = [];
 		foreach($settings as $key => $values) {
 			$sql = self::setSetting($moduleDirectoryPrefix, $pid, $key, $values);
-
 			if(!empty($sql)){
 				$saveSqlByField[$key] = $sql;
 			}
 		}
-
 		return $saveSqlByField;
 	}
 
